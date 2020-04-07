ab39b1e27f4ea0552b4fa8c473475b579ff6a2d3
Rohith <rohithbodagala.rit@gmail.com>
2020-04-07 09:13:26 +0530

    Init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const request = require('request')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+const address=process.argv[2]
+if(!address){
+    console.log('Please provide an address!')
+}
+else{
+    //GeoCoding
+    geocode(address,(error,{latitude,longitude,location})=>{
+
+        if(error){
+            return console.log(error)
+        }
+        
+        //Forecasting
+        forecast(latitude,longitude, (error, forecastData) => {
+        if(error)
+        {
+            return console.log(error)
+        }
+
+        console.log(location)
+        console.log(forecastData)
+    })
+})
+    
+}
+
+
+
